2a320f926ad57a7b89ec7274d5581bc712f11bca
Kazishayem <kazishayem1234@gmail.com>
2022-05-16 17:58:26 +0600

    aas

--- JS/index.js
+++ JS/index.js
@@ -73,3 +73,53 @@ var typed = new Typed(".typing-2", {
         
     });
 });
+
+window.addEventListener("DOMContentLoaded", function() {
+
+    // get the form elements defined in your form HTML above
+    
+    var form = document.getElementById("my-form");
+    var button = document.getElementById("my-form-button");
+    var status = document.getElementById("my-form-status");
+
+    // Success and Error functions for after the form is submitted
+    
+    function success() {
+      form.reset();
+      status.innerHTML = "<span class='sent-message'>Your message has been sent. Thank you!</span>";
+    }
+
+    function error() {
+      status.innerHTML = "<span class='error-message'>Oops! There was a problem  &times;</span>";
+    }
+    $(document).ready(function(){
+      $("#my-form-status").click(function(){
+        status.innerHTML ="<span style='display:none;'> </span>";
+      });
+    });
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function(ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+  });
+  
+  // helper function for sending an AJAX request
+
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
